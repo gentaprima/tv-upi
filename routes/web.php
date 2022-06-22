f1df509c16235ff5e954466a8d3d95877041195a
gentaprima <gentaprima600@gmail.com>
2022-06-22 17:46:30 +0700

    fix middleware login

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Session;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,45 +15,52 @@ use Illuminate\Support\Facades\Route;
 */
 
 // login
-Route::get('/','LoginController@index');
-Route::post('/auth','LoginController@auth');
+Route::get('/', 'LoginController@index');
+Route::post('/auth', 'LoginController@auth');
 
-// dashboard
-Route::get('/beranda','DashboardController@index');
+Route::group(['middleware' => 'check.login', 'prefix' => '/'], function () {
+    // dashboard
+    Route::get('/beranda', 'DashboardController@index');
 
-// banner
-Route::get('/data-banner','DashboardController@getBanner');
-Route::post('/add-banner','BannerController@store');
-Route::post('/update-banner/{id}','BannerController@update');
-Route::get('/delete-banner/{id}','BannerController@destroy');
+    // banner
+    Route::get('/data-banner', 'DashboardController@getBanner');
+    Route::post('/add-banner', 'BannerController@store');
+    Route::post('/update-banner/{id}', 'BannerController@update');
+    Route::get('/delete-banner/{id}', 'BannerController@destroy');
 
-// video
-Route::get('/data-video','DashboardController@getVideo');
-Route::post('/add-video','VideoController@store');
-Route::post('/update-video/{id}','VideoController@update');
-Route::get('/delete-video/{id}','VideoController@destroy');
+    // video
+    Route::get('/data-video', 'DashboardController@getVideo');
+    Route::post('/add-video', 'VideoController@store');
+    Route::post('/update-video/{id}', 'VideoController@update');
+    Route::get('/delete-video/{id}', 'VideoController@destroy');
 
-// iklan
-Route::get('/data-iklan/{id}','DashboardController@getIklan');
-Route::post('/add-ads','AdsController@store');
-Route::post('/update-ads/{id}','AdsController@update');
-Route::get('/delete-ads/{id}','AdsController@destroy');
+    // iklan
+    Route::get('/data-iklan/{id}', 'DashboardController@getIklan');
+    Route::post('/add-ads', 'AdsController@store');
+    Route::post('/update-ads/{id}', 'AdsController@update');
+    Route::get('/delete-ads/{id}', 'AdsController@destroy');
 
-// berita & kategori
-Route::get('/data-kategori-berita','DashboardController@getKategoriBerita');
-Route::post('add-kategori-berita','KategoriBeritaController@store');
-Route::post('update-kategori-berita/{id}','KategoriBeritaController@update');
-Route::get('delete-kategori-berita/{id}','KategoriBeritaController@destroy');
-Route::get('/data-berita','DashboardController@getBerita');
-Route::post('/add-berita','BeritaController@store');
-Route::post('/update-berita/{id}','BeritaController@update');
-Route::post('/delete-berita/{id}','BeritaController@destroy');
-Route::get('/show-berita/{id}','BeritaController@show');
+    // berita & kategori
+    Route::get('/data-kategori-berita', 'DashboardController@getKategoriBerita');
+    Route::post('add-kategori-berita', 'KategoriBeritaController@store');
+    Route::post('update-kategori-berita/{id}', 'KategoriBeritaController@update');
+    Route::get('delete-kategori-berita/{id}', 'KategoriBeritaController@destroy');
+    Route::get('/data-berita', 'DashboardController@getBerita');
+    Route::post('/add-berita', 'BeritaController@store');
+    Route::post('/update-berita/{id}', 'BeritaController@update');
+    Route::post('/delete-berita/{id}', 'BeritaController@destroy');
+    Route::get('/show-berita/{id}', 'BeritaController@show');
 
-// jadwal siaran
-Route::get('/jadwal-siaran','DashboardController@jadwalSiaran');
-Route::get('/ubah-siaran/{id}','DashboardController@ubahSiaran');
-Route::post('/add-jadwal-siaran','JadwalSiaranController@store');
-Route::post('/update-jadwal-siaran/{id}','JadwalSiaranController@update');
-Route::get('/delete-jadwal-siaran/{id}','JadwalSiaranController@destroy');
-Route::get('/jadwal-siaran-show/{id}','JadwalSiaranController@show');
+    // jadwal siaran
+    Route::get('/jadwal-siaran', 'DashboardController@jadwalSiaran');
+    Route::get('/ubah-siaran/{id}', 'DashboardController@ubahSiaran');
+    Route::post('/add-jadwal-siaran', 'JadwalSiaranController@store');
+    Route::post('/update-jadwal-siaran/{id}', 'JadwalSiaranController@update');
+    Route::get('/delete-jadwal-siaran/{id}', 'JadwalSiaranController@destroy');
+    Route::get('/jadwal-siaran-show/{id}', 'JadwalSiaranController@show');
+
+    Route::get('/logout', function () {
+        Session::flush();
+        return redirect('/');
+    });
+});

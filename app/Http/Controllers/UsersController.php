dd7c577d577c89f51e043d3f0a35f9f523d617a8
gentaprima <gentaprima600@gmail.com>
2022-06-24 23:53:40 +0700

    fix add-users

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -8,13 +8,17 @@ use Illuminate\Support\Facades\DB;
 class UsersController extends Controller
 {
     public function store(Request $request){
-        $users = [
-            'email' => $request->email,
-            'nama_lengkap' => $request->fullName,
-            'role'  => 0
-        ];
+        $cekUsers = DB::table('tbl_users')->where('email','=',$request->email)->first();
 
-        DB::table('tbl_users')->insert($users);
+        if($cekUsers == null){
+            $users = [
+                'email' => $request->email,
+                'nama_lengkap' => $request->fullName,
+                'role'  => 0
+            ];
+    
+            DB::table('tbl_users')->insert($users);
+        }
         return response()->json([
             'message' => 'sukses',
             'status' => true,
